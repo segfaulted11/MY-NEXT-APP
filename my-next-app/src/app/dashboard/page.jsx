ab8b3afc9b4777b97f17059327ba76311781c8ed
Kazi Rakin Bin Zaman <segfaulted11@gmail.com>
2026-06-07 14:13:16 +0000

    layout

---
+++ my-next-app/src/app/dashboard/page.jsx
@@ -0,0 +1,10 @@
+
+const  DashBoardPage = () => {
+    return (
+        <div>
+            <h1>Dashboard page</h1>
+        </div>
+    );
+};
+
+export default DashBoardPage;
---
+++ my-next-app/src/app/dashboard/revenue/page.jsx
@@ -0,0 +1,10 @@
+
+const Profile = () => {
+    return (
+        <div>
+            <h2>revenue page</h2>
+        </div>
+    );
+};
+
+export default Profile;
