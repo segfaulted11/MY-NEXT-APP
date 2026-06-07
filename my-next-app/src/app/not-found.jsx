617cfad3062819802e7e74d337a77cd2a77696f8
Kazi Rakin Bin Zaman <segfaulted11@gmail.com>
2026-06-07 14:38:23 +0000

    UI for not found page

---
+++ my-next-app/src/app/not-found.jsx
@@ -0,0 +1,13 @@
+
+
+const NotFound = () => {
+return (
+        <div className="text-red-600">
+            <h2>Page Not Found</h2>
+            <p>The page you are looking for does not exist...</p>
+        </div>
+        // create a "not-found.jsx" file in the root folder (app folder) for pages that doesnt exist
+    );
+};
+
+export default NotFound;
